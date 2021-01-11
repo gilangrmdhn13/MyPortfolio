813b6ddfc442ec61a978ec68f00ba174023e38e3
Gilang <gilangocon@gmail.com>
2021-01-11 21:18:34 +0700

    initial commit add Navbar component

---
+++ src/components/navbar/mynavbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
+
+const MyNavbar = () => {
+  return (
+    <>
+      <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark" className="animate-navbar nav-theme justify-content-between ">
+        <Navbar.Brand href="#home">
+          <h4>Gilang Ramadhan Hatno</h4>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  )
+}
+
+export default MyNavbar
---
+++ src/themes/theme.js
@@ -0,0 +1,4 @@
+export const theme = {
+  primary: "#0B0C10",
+  textColor: "#66FCF1",
+};
